a7d8074585482540a950c1aa719f825ba6dc3f83
ArthurRyoo <jene0523@gmail.com>
2020-10-08 23:53:19 -0700

    Add Habits component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Habit from './components/Habit';
+import Habits from './components/Habits';
 
 function App() {
   return (
-    <Habit />
+    <Habits />
   );
 }
 
